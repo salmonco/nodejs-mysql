dd362ed38583a91b01bb2b96e48503f38536e8a4
salmonco <xkqjsslsek80@naver.com>
2024-02-05 17:35:36 +0900

    Refactor: 변수명 변경

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -10,19 +10,17 @@ router.post("/signup", async (req, res) => {
   try {
     const user = req.body;
 
-    const response = user;
-    response.Status = false;
-
     const db = await mysql.getConnection();
     const sql =
       "INSERT INTO user (user_id, password, nickname, gender) VALUES(?,?,?,?)";
     const params = [user.ID, user.Password, user.Nickname, user.Gender];
     const [rows, fields] = await db.query(sql, params);
 
-    response.Status = true;
-    response.UserID = rows.insertId;
+    const responseBody = user;
+    responseBody.Status = true;
+    responseBody.UserID = rows.insertId;
 
-    res.send(response);
+    res.send(responseBody);
   } catch (error) {
     console.error("signup", error);
     res.status(503);
@@ -39,22 +37,23 @@ router.post("/signin", async (req, res) => {
   try {
     const { ID, Password } = req.body;
 
-    const response = {};
-    response.Status = false;
-
     const db = await mysql.getConnection();
     const sql = "SELECT * FROM user WHERE user_id = ? AND password = ?";
     const params = [ID, Password];
     const [rows, fields] = await db.query(sql, params);
 
+    const responseBody = {};
+
     if (rows.length) {
-      response.Status = true;
-      response.UserID = rows[0].id;
-      response.Nickname = rows[0].nickname;
-      response.Gender = rows[0].gender;
+      responseBody.Status = true;
+      responseBody.UserID = rows[0].id;
+      responseBody.Nickname = rows[0].nickname;
+      responseBody.Gender = rows[0].gender;
+    } else {
+      responseBody.Status = false;
     }
 
-    res.send(response);
+    res.send(responseBody);
   } catch (error) {
     console.error("signin", error);
     res.status(503);
@@ -71,17 +70,15 @@ router.post("/checkDuplicateID", async (req, res) => {
   try {
     const ID = req.body.ID;
 
-    const response = {};
-    response.Status = false;
-
     const db = await mysql.getConnection();
     const sql = "SELECT * FROM user WHERE user_id = ?";
     const params = [ID];
     const [rows, fields] = await db.query(sql, params);
 
-    if (rows.length === 0) response.Status = true;
+    const responseBody = {};
+    responseBody.Status = rows.length === 0;
 
-    res.send(response);
+    res.send(responseBody);
   } catch (error) {
     console.error("checkDuplicateID", error);
     res.send(error);
@@ -97,17 +94,15 @@ router.post("/checkDuplicateName", async (req, res) => {
   try {
     const nickname = req.body.Nickname;
 
-    const response = {};
-    response.Status = false;
-
     const db = await mysql.getConnection();
     const sql = "SELECT * FROM user WHERE nickname = ?";
     const params = [nickname];
     const [rows, fields] = await db.query(sql, params);
 
-    if (rows.length === 0) response.Status = true;
+    const responseBody = {};
+    responseBody.Status = rows.length === 0;
 
-    res.send(response);
+    res.send(responseBody);
   } catch (error) {
     console.error("checkDuplicateName", error);
     res.send(error);
